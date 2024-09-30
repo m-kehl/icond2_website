a9397544c4e4b14b2f84c9e8f6db95e7ea9eafad
m-kehl <rudolfsinger36@gmail.com>
2024-09-30 14:25:29 +0200

    relocate html code for impressum/datenschutz/memory

---
+++ html_impressum.js
@@ -0,0 +1,32 @@
+    document.write(`<img class="frog_imp tablink" src = "frog_imgs/laubfrosch_tuerkis.png", onclick="changefrog()" ondblclick="openSubpage(event,'memory')"> 
+        <h2>Impressum</h2>
+        <h3>Kontakt</h3>
+        <div>M. Kehl <br> mkehl.laubfrosch@gmail.com <br> Quellcode auf <a class="link_frog" href="https://github.com/m-kehl/weather_icond2" target="_blank">GitHub</a></div>
+ 
+        <h3>Haftungsausschluss</h3>
+        <div>Der Autor übernimmt keine Gewähr für die Richtigkeit, Genauigkeit,
+            Aktualität, Zuverlässigkeit und Vollständigkeit der Informationen.
+            Haftungsansprüche gegen den Autor wegen Schäden materieller oder
+            immaterieller Art, die aus dem Zugriff oder der Nutzung bzw.
+            Nichtnutzung der veröffentlichten Informationen, durch Missbrauch
+            der Verbindung oder durch technische Störungen entstanden sind,
+            werden ausgeschlossen. <br>
+            Alle Angebote sind freibleibend. Der Autor behält es sich
+            ausdrücklich vor, Teile der Seiten oder das gesamte Angebot ohne
+            gesonderte Ankündigung zu verändern, zu ergänzen, zu löschen oder
+            die Veröffentlichung zeitweise oder endgültig einzustellen.
+            </div>
+        <h3>Haftungsausschluss für Inhalte und Links</h3>
+        <div>Verweise und Links auf Webseiten Dritter liegen ausserhalb
+            unseres Verantwortungsbereichs. Es wird jegliche Verantwortung
+            für solche Webseiten abgelehnt. Der Zugriff und die Nutzung
+            solcher Webseiten erfolgen auf eigene Gefahr des jeweiligen Nutzers.</div>
+        <h3>Urheberrechtserklärung</h3>
+        <div>Die Urheber- und alle anderen Rechte an Inhalten, Bildern, Fotos
+            oder anderen Dateien auf dieser Website, gehören ausschliesslich
+            den genannten Rechteinhabern. Für die Reproduktion jeglicher
+            Elemente ist die schriftliche Zustimmung des Urheberrechtsträgers
+            im Voraus einzuholen.</div>
+        <h3>Quelle Impressum</h3>
+        <a class="link_frog" href="https://brainbox.swiss/impressum-generator-schweiz/" target="_blank">BrainBox Solutions</a>
+        <br><br><br></br>`)
---
+++ html_memory.js
@@ -0,0 +1,173 @@
+document.write(`
+           <div class="nonmemory">
+            Der Bildschirm ist zu klein, um das Froschmemory spieln zu können.
+        </div>
+        <div class="playground">
+            <div class="card" >
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch.png" alt="picture frog" class="frog_pic frog_img" id = "img0">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_blau.png" alt="picture frog" class="frog_pic frog_img" id = "img1">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_gelb.png" alt="picture frog" class="frog_pic frog_img" id = "img2">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_gruen.png" alt="picture frog" class="frog_pic frog_img" id = "img3">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_dunkelgruen.png" alt="picture frog" class="frog_pic frog_img" id = "img4">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_pink.png" alt="picture frog" class="frog_pic frog_img" id = "img5">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_rot.png" alt="picture frog" class="frog_pic frog_img" id = "img6">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_tuerkis.png" alt="picture frog" class="frog_pic frog_img" id = "img7">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch.png" alt="picture frog" class="frog_pic frog_img" id = "img8">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_blau.png" alt="picture frog" class="frog_pic frog_img" id = "img9">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_gelb.png" alt="picture frog" class="frog_pic frog_img" id = "img10">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_gruen.png" alt="picture frog" class="frog_pic frog_img" id = "img11">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_dunkelgruen.png" alt="picture frog" class="frog_pic frog_img" id = "img12">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_pink.png" alt="picture frog" class="frog_pic frog_img" id = "img13">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_rot.png" alt="picture frog" class="frog_pic frog_img" id = "img14">
+                </div>
+            </div>
+            </div>
+            <div class="card">
+            <div class="inner-card">
+                <div class="front">
+                    <img src="frog_imgs/front_pic.png" alt="front image" class="front_img frog_img">
+                </div>
+                <div class="back">
+                    <img src="frog_imgs/laubfrosch_tuerkis.png" alt="picture frog" class="frog_pic frog_img" id = "img15">
+                </div>
+            </div>
+            </div>
+        
+        <div class="field">
+            <div class="field_box" id = pairs>Found pairs: 0</div>
+            <div class="field_box" id = "tries">Your tries: 0</div>
+            <button class="field_box newgame" onclick="f_newgame()">New Game</button>
+        </div>
+
+    `)
