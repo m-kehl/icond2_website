a6cb9b45628984c68de4830a57a0b5fbc961c6b7
m-kehl <rudolfsinger36@gmail.com>
2024-10-11 11:46:11 +0200

    refresh link to GitHub

--- html_impressum.js
+++ html_impressum.js
@@ -1,7 +1,7 @@
     document.write(`<img class="frog_imp tablink" src = "frog_imgs/laubfrosch_tuerkis.png", onclick="changefrog()" ondblclick="openSubpage(event,'memory')"> 
         <h2>Impressum</h2>
         <h3>Kontakt</h3>
-        <div>M. Kehl <br> mkehl.laubfrosch@gmail.com <br> Quellcode auf GitHub: 
+        <div>M. Kehl <br> mkehl.laubfrosch@gmail.com <br> <br> Quellcode auf GitHub:
         <a class="link_frog" href="https://github.com/m-kehl/icond2_website" target="_blank">Website</a> und 
         <a class="link_frog" href="https://github.com/m-kehl/weather_icond2" target="_blank">RShiny</a></div>
  
